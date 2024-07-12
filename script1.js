9c69fe0728a6a415ffb5e44de65bf2f88ee0f79e
TECH SPIDER <techspider1610@gmail.com>
2024-07-12 21:56:19 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const togglePassword = document.getElementById('togglePassword');
+  const password = document.getElementById('password');
+
+  togglePassword.addEventListener('click', function (e) {
+  
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    
+    
+    this.classList.toggle('fa-eye-slash');
+  });
+});
